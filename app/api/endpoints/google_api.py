841a9e9b70d6fe8fd035dfc6ce34259b9c7fe5fa
Vladimir Maksimov <111915960+v-mcsimoff@users.noreply.github.com>
2023-08-30 20:36:29 +0200

    Update google_api.py

--- app/api/endpoints/google_api.py
+++ app/api/endpoints/google_api.py
@@ -23,7 +23,7 @@ async def get_report(
         session: AsyncSession = Depends(get_async_session),
         wrapper_services: Aiogoogle = Depends(get_service)
 ):
-    """Только для суперюзеров."""
+    """For superusers only."""
     projects = await charity_project_crud.get_projects_by_completion_rate(session)
     spreadsheetid = await spreadsheets_create(wrapper_services)
     await set_user_permissions(spreadsheetid, wrapper_services)
